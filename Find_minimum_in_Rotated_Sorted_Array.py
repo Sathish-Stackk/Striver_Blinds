7364d05d4cba820101fff40cffc0cdc42fb29d68
Mungi Sathish <sathishmungi@gmail.com>
2026-08-05 20:35:33 +0530

    Create Find_minimum_in_Rotated_Sorted_Array.py

---
+++ Find_minimum_in_Rotated_Sorted_Array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findMin(self,nums):
+
+        left = 0
+        right= len(nums)-1
+
+        while left < right:
+
+            mid =(left+right)//2
+
+            if nums[mid] > nums[right]:
+                left = mid+1
+
+            else:
+                right =mid
+        return nums[left]
+
+
+#Time Complexity: O(log n)#
+
+# Space Complexity: O(1)
