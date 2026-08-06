40b86a8e282d86b2f08119e7cf5a81279ebb7ca9
Mungi Sathish <sathishmungi@gmail.com>
2026-08-06 23:23:30 +0530

    Add complexity analysis comments to the function
    Added comments explaining time and space complexity.

---
+++ Container_with_most_water.py
@@ -0,0 +1,36 @@
+class Solution:
+    def maxArea(self, height):
+
+        left = 0
+        right = len(height) - 1
+
+        max_area = 0
+
+        while left < right:
+
+            width = right - left
+
+            current_height = min(height[left], height[right])
+
+            area = width * current_height
+
+            max_area = max(max_area, area)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_area
+
+
+# Time Complexity
+
+# Each pointer moves at most n times.
+
+# Time = O(n)
+# Space Complexity
+
+# Only a few variables are used.
+
+# Space = O(1)
