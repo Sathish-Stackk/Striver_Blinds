d627cc273b9e5419e73e5a0ea276161f38e54e7c
Mungi Sathish <sathishmungi@gmail.com>
2026-08-08 22:55:24 +0530

    Add missing number solution using XOR
    Implement a solution to find the missing number in an array using XOR.

---
+++ Missing_number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def missingNumber(self, nums):
+        n = len(nums)
+        xor = n
+
+        for i in range(n):
+            xor ^= i
+            xor ^= nums[i]
+
+        return xor
+
+#   Complexity
+# Time  : O(n)
+# Space : O(1)
